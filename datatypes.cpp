cb65c08bf11dcd6d6cd896e0044f5cb34981fdee
mohammedsharukhh <149393894+mohammedsharukhh@users.noreply.github.com>
2024-03-02 04:53:10 +0000

    Data Types

---
+++ datatypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 18;
+    cout << a << endl ;
+    char ch = 'a';
+    cout << ch << endl ;
+    bool b = true ;
+    cout << b << endl ;
+    double D = 1.86;
+    cout << D << endl ;
+    float v = 1.2;
+    cout << v << endl;
+}
