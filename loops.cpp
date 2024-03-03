a781fe58ff1b7dba28262bdd9c35cdf09892f0d5
mohammedsharukhh <149393894+mohammedsharukhh@users.noreply.github.com>
2024-03-03 15:27:08 +0000

    ifelse

---
+++ loops.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main () {
+   /*/int a;
+    cout << "Enter the value of A: ";
+    cin >> a ;
+  
+
+    if ( a > 0)
+    {
+        cout << "A is positive" << endl ;
+
+    }
+    else {
+        cout << "A is negative" << endl;
+    }
+    /*/
+    /*
+    int a;
+    int b;
+    cout << " Enter the value of a: ";
+    cin >> a;
+    cout << " Enter the value of b: ";
+    cin >> b;
+    if (a > b){
+        cout << "a is greater" << endl;
+    }
+    else {
+        cout << "b is greater" <<endl;
+    }
+    return 0; */
+
+    
+
+}
