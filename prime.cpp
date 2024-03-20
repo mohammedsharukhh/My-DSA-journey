82b76f00adb12ca7ab23a42e385563ce7550f251
mohammedsharukhh <149393894+mohammedsharukhh@users.noreply.github.com>
2024-03-20 16:41:13 +0000

    day_3

---
+++ prime.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    int i = 2;
+
+    while ( i < n)
+    {
+        if(n%i==0) {
+            cout <<"Not prime" << i << endl;
+        }
+        else{
+            cout <<"prime "<< i << endl;
+        }
+          i = i + 1;
+    }
+    
+}
