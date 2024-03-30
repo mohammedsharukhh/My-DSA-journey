bbd6c6bb2d708407b52c0a27c31d75cd7425b1b2
mohammedsharukhh <149393894+mohammedsharukhh@users.noreply.github.com>
2024-03-30 03:02:38 +0000

    patterns solving

---
+++ pattern21.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int row = 1;
+    int n;
+    cout << "Enterr the value of n: ";
+    cin >> n;
+    while (row <= n)
+    {
+        int space = n - row;
+        while (space)
+        {
+            cout <<" ";
+            space = space - 1;
+        }
+        
+        int col = 1;
+        while (col <= row)
+        {
+            cout << "*";
+            col = col + 1;
+
+            
+        }
+        cout << endl;
+        row = row + 1;
+    }
+    
+
+}
