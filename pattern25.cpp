c6969e88820c5f15d717cb3525f9f070d8621fca
mohammedsharukhh <149393894+mohammedsharukhh@users.noreply.github.com>
2024-03-31 05:09:02 +0000

    solving last dabang pattern

---
+++ pattern25.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int row = 1;
+    int n = 5;
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= n - row + 1)
+        {
+            cout << col;
+            col = col + 1;
+        }
+      
+        int start = 1;
+        while (start < row )
+        {
+           cout << "*";
+           start = start + 1;
+        }    
+        int space = row - 1;
+        while (space)
+        {
+            cout << "*";
+            space = space - 1;
+        }
+        int cole = 1;
+        while (cole <= n - row)
+        {
+           cout << col;
+           cole = cole + 1;
+        }
+           
+        cout << endl;
+        row = row + 1;
+        
+    }
+    
+}
